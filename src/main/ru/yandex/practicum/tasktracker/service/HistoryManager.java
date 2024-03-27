31af172e71e02979c464edff6cdc62dced5a6837
User Petyan <petiti14771@mail.ru>
2024-03-28 01:21:00 +0300

    Add list to my linked list in history

--- src/main/ru/yandex/practicum/tasktracker/service/HistoryManager.java
+++ src/main/ru/yandex/practicum/tasktracker/service/HistoryManager.java
@@ -1,12 +1,17 @@
 package ru.yandex.practicum.tasktracker.service;
 
 import ru.yandex.practicum.tasktracker.model.Task;
+import ru.yandex.practicum.tasktracker.util.Node;
 
 import java.util.List;
 
 public interface HistoryManager {
 
     void add(Task task);
+
+    void linkLast(Task task);
+
+    void remove(int id);
 
     List<Task> getHistory();
 }
--- src/main/ru/yandex/practicum/tasktracker/service/InMemoryHistoryManager.java
+++ src/main/ru/yandex/practicum/tasktracker/service/InMemoryHistoryManager.java
@@ -1,26 +1,87 @@
 package ru.yandex.practicum.tasktracker.service;
-
 import ru.yandex.practicum.tasktracker.model.Task;
+import ru.yandex.practicum.tasktracker.util.Node;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private final List<Task> tasks = new ArrayList<>(11);
+    private Node head;
+    private final HashMap<Integer, Node> map = new HashMap<>();
 
     @Override
-    public void add(Task task) {//я понял о чем ты, спасибо, правда безполезно там ифы делаю
+    public void add(Task task) {
         if (task != null) {
-            tasks.add(task);
-        }
-        if (tasks.size() > 10) {
-            tasks.remove(0);
+            if (map.get(task.getId()) != null) {
+                removeNode(map.get(task.getId()));
+            }
+            linkLast(task);
         }
     }
 
     @Override
+    public void linkLast(Task task) {
+        Node newNode = new Node(task);
+        if (map.get(task.getId()) != null) {
+            Node currentNode = map.get(task.getId());
+            Node nextNode = currentNode.getNext();
+            Node lastNode = currentNode.getPrev();
+            newNode.setPrev(lastNode);
+            lastNode.setNext(nextNode);
+            map.remove(task.getId());
+        }
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node tempNode = head;
+            while (tempNode.getNext() != null) {
+                tempNode = tempNode.getNext();
+            }
+            tempNode.setNext(newNode);
+            newNode.setPrev(tempNode);
+        }
+        map.put(task.getId(), newNode);
+    }
+
+    @Override
+    public void remove(int id) {
+        Node currentNode = map.get(id);
+        if (currentNode != null) {
+            removeNode(currentNode);
+        }
+    }
+
+    public void removeNode(Node node) {
+        Node currentNode = map.get(node.getData().getId());
+        if (currentNode.getPrev() != null) {
+            currentNode.getPrev().setNext(currentNode.getNext());
+        }
+        if (currentNode.getNext() != null) {
+            currentNode.getNext().setPrev(currentNode.getPrev());
+        }
+        map.remove(node.getData().getId());
+    }
+
+    @Override
     public List<Task> getHistory() {
-        return tasks;
+        List<Task> history = new ArrayList<>();
+        for (Node node : map.values()) {
+            history.add(node.getData());
+        }
+        return history;
+    }
+
+    private void remove(Node node) {
+        if (map.get(node.getData().getId()) == null) {
+            throw new RuntimeException("Такого элемента не существует");
+        }
+        Node currentNode = map.get(node.getData().getId());
+        Node nextNode = currentNode.getNext();
+        Node lastNode = currentNode.getPrev();
+        nextNode.setPrev(lastNode);
+        lastNode.setNext(nextNode);
+        map.remove(node.getData().getId());
     }
 }
